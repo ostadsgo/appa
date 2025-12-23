f3c8f21ff5f4b51405074fe9bdd21baa1f777a0f
Saeed Gholami <saeed.ghollami@gmail.com>
2025-12-23 07:25:45 +0330

    Sort package by date time.

--- appa.py
+++ appa.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 from datetime import datetime, timedelta, timezone
 
+
 # Run pacman -Q to get the list of installed packages
 def get_pkgs_metadata() -> str:
     metadata = None
@@ -23,28 +24,18 @@ def get_packages(metadata: str) -> list[str]:
     return [pkg for pkg in pkgs if pkg]
 
 
-def extract_depend_on(value: str) -> list[str]:
-    return value.split("  ")
-
-
-def extract_optional_deps(value: str) -> list[str]:
-    return value.split("  ")
-
-
-
-
 def convert_to_data_type(pkg: dict[str, str]):
     pkg_data = {}
     for name, value in pkg.items():
-        if name in ["depends on",  "optional deps"]:
+        if name in ["depends on", "optional deps"]:
             value = value.split("  ")
         elif name in ["build date", "install date"]:
-            datetime_format ='%a %d %b %Y %I:%M:%S %p %z'
+            datetime_format = "%a %d %b %Y %I:%M:%S %p %z"
             value = datetime.strptime(value, datetime_format)
 
         pkg_data[name] = value
     return pkg_data
-        
+
 
 def parse_package(raw_package: str) -> dict[str, str]:
     """Parse and extract fields of a package."""
@@ -78,11 +69,17 @@ def parse_packages(packages):
 
     return packages_list
 
+def sort_data_by_datetime(packages):
+    return sorted(packages, key=lambda x: x["install date"], reverse=True)
+
+
 def main():
     metadata = get_pkgs_metadata()
     row_data = get_packages(metadata)
     packages = parse_packages(row_data)
-    print(len(packages))
+    sorted_pkgs = sort_data_by_datetime(packages)
+    print(sorted_pkgs[40])
+
 
 if __name__ == "__main__":
     main()
